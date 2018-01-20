781b84c8fb748504954eadca3e5f3575fc4182a5
tienbk97 <nghuuanhtien@gmail.com>
2018-01-20 15:51:44 +0700

    get location ok

--- app/src/main/java/hoangloc/parkinglotfinder/ActivityMain.java
+++ app/src/main/java/hoangloc/parkinglotfinder/ActivityMain.java
@@ -1,9 +1,26 @@
 package hoangloc.parkinglotfinder;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.*;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
@@ -21,18 +38,94 @@ import android.util.DisplayMetrics;
 import android.content.res.Configuration;
 import java.util.Locale;
 
-public class ActivityMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class ActivityMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
+        OnMapReadyCallback,
 
+        GoogleApiClient.ConnectionCallbacks,
+
+        GoogleApiClient.OnConnectionFailedListener,
+
+        LocationListener {
+
+
+
+    public GoogleMap mMap;
+    private GoogleApiClient client;
+    private LocationRequest locationRequest;
+    public FloatingActionButton fab;
+    LatLng latLng;
+    private static final int REQUEST_CODE_PERMISSION = 2;
+    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
     static SharedPreferences sharedPref;
     static SharedPreferences.Editor prefEditor;
     static int currentLang;
     static int currentTheme;
     int themeColor;
     int headerImage;
-
+    private long interval ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        SupportMapFragment mapFragment = new com.google.android.gms.maps.SupportMapFragment();
+        ft.replace(R.id.place_holder_MainActivity, mapFragment);
+        ft.commit();
+        /*SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+
+                .findFragmentById(R.id.map);*/
+
+        mapFragment.getMapAsync(this);
+
+        try {
+
+            if (ActivityCompat.checkSelfPermission(this, mPermission)
+
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]{mPermission},
+
+                        REQUEST_CODE_PERMISSION);
+
+
+
+                // If any permission above not allowed by user, this condition will
+
+                //execute every time, else your else part will work
+
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+        FloatingActionButton fabLoc = (FloatingActionButton) findViewById(R.id.buttonGetLocation);
+
+        fabLoc.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                try {
+
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+                }
+
+                catch (Exception e){
+
+                    Snackbar.make(fab,"LOCATION NOT FOUND\nPlease turn on Location Services",Snackbar.LENGTH_SHORT).show();
+
+                }
+
+            }
+
+        });
+
+
         sharedPref = getSharedPreferences(getString(R.string.preperences_file),this.MODE_PRIVATE);
         prefEditor = sharedPref.edit();
         setLanguage();
@@ -41,7 +134,7 @@ public class ActivityMain extends AppCompatActivity implements NavigationView.On
         //themeColor = sharedPref.getInt("currentColor",R.color.colorPrimary);
         //headerImage = sharedPref.getInt("currentHeader",R.drawable.header);
 
-        setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -130,5 +223,77 @@ public class ActivityMain extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        locationRequest = new LocationRequest();
+
+        locationRequest.setInterval(interval);
+
+        locationRequest.setFastestInterval(100);
+
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+
+
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(client, locationRequest, this);
+
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double latitude = location.getLatitude();
+
+        double longitude = location.getLongitude();
+
+        latLng = new LatLng(latitude,longitude);
+    }
+    private void buildGoogleApiClient() {
+
+        client = new GoogleApiClient.Builder(this)
+
+                .addConnectionCallbacks(this)
+
+                .addOnConnectionFailedListener(this)
+
+                .addApi(LocationServices.API)
+
+                .build();
+
+
+
+        client.connect();
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+
+            buildGoogleApiClient();
+
+            mMap.setMyLocationEnabled(true); // cai nay de cho phep lay location, dat trong vong if de coi coi may da duoc cho quen su dung GPS hay chua
+
+        }
+
+
     }
 }
--- app/src/main/java/hoangloc/parkinglotfinder/FragmentHome.java
+++ app/src/main/java/hoangloc/parkinglotfinder/FragmentHome.java
@@ -1,28 +1,133 @@
 package hoangloc.parkinglotfinder;
-import android.os.Bundle;
+
+import android.Manifest;
+
+import android.content.Context;
+
+import android.content.Intent;
+
+import android.content.SharedPreferences;
+
+import android.content.pm.PackageManager;
+
+import android.graphics.Bitmap;
+
+import android.graphics.Point;
+
+import android.location.Address;
+
+import android.location.Geocoder;
+
+import android.location.Location;
+
+
+
+import android.os.Build;
+
+import android.os.Handler;
+
+import android.provider.Settings;
+
+import android.support.annotation.NonNull;
+
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
+
+import android.support.design.widget.FloatingActionButton;
+
+import android.support.design.widget.NavigationView;
+
+import android.support.design.widget.Snackbar;
+
+import android.support.v4.app.ActivityCompat;
+
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentActivity;
+
+import android.os.Bundle;
+
+
+
+import android.support.v4.content.ContextCompat;
+
+import android.support.v4.widget.DrawerLayout;
+
+import android.support.v7.app.ActionBarDrawerToggle;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.widget.LinearLayoutManager;
+
+import android.support.v7.widget.RecyclerView;
+
+import android.telephony.TelephonyManager;
+
+import android.view.Display;
+
 import android.view.LayoutInflater;
+import android.view.MenuItem;
+
 import android.view.View;
+
 import android.view.ViewGroup;
+import android.widget.*;
+
+import com.google.android.gms.common.ConnectionResult;
+
+import com.google.android.gms.location.LocationListener;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import com.google.android.gms.location.LocationRequest;
+
+import com.google.android.gms.location.LocationServices;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
+
 import com.google.android.gms.maps.GoogleMap;
+
 import com.google.android.gms.maps.OnMapReadyCallback;
+
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * Created by NguyenHoangLoc on 1/20/2018.
- */
+import com.google.android.gms.maps.model.*;
+
+import java.io.IOException;
+
+import java.lang.reflect.Array;
+
+import java.util.*;
 
 
-public class FragmentHome extends Fragment implements OnMapReadyCallback {
 
-    GoogleMap mMap;
+public class FragmentHome extends Fragment implements
+
+        OnMapReadyCallback,
+
+        GoogleApiClient.ConnectionCallbacks,
+
+        GoogleApiClient.OnConnectionFailedListener,
+
+        LocationListener {
+
+
+
+    public GoogleMap mMap;
+
+    private GoogleApiClient client;
+
+    private LocationRequest locationRequest;
+
+    public HashMap<String,Marker> currentMarker = new HashMap<>();
+
+    //public FloatingActionButton fab;
+
+    LatLng latLng;
+
+    private static final int REQUEST_CODE_PERMISSION = 2;
+
+    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+
+    private long interval ;
 
     public FragmentHome() {
         // Required empty public constructor
@@ -30,25 +135,134 @@ public class FragmentHome extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_home,null);
+        View v = inflater.inflate(R.layout.fragment_setting,null);
+
 
         return v;
-}
+    }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         SupportMapFragment mapFragment =
+
                 (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.fragment_map);
+
         mapFragment.getMapAsync(this);
+        try {
+
+            if (ActivityCompat.checkSelfPermission(getContext(), mPermission)
+
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{mPermission},REQUEST_CODE_PERMISSION);
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+
+    public void onMapReady(GoogleMap googleMap) { // cai nay duoc goi moi khi map san sang de duoc su dung
+
         mMap = googleMap;
 
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+
+
+        if (ActivityCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+
+            buildGoogleApiClient();
+
+            mMap.setMyLocationEnabled(true); // cai nay de cho phep lay location, dat trong vong if de coi coi may da duoc cho quen su dung GPS hay chua
+
+        }
+
+
+
     }
+
+
+
+
+
+    private void buildGoogleApiClient() {
+
+        client = new GoogleApiClient.Builder(getContext())
+
+                .addConnectionCallbacks(this)
+
+                .addOnConnectionFailedListener(this)
+
+                .addApi(LocationServices.API)
+
+                .build();
+
+
+
+        client.connect();
+
+    }
+
+
+
+    @Override
+
+    public void onLocationChanged(Location location) {
+
+        double latitude = location.getLatitude();
+
+        double longitude = location.getLongitude();
+
+        latLng = new LatLng(latitude,longitude);
+
+
+
+    }
+
+
+
+    @Override
+
+    public void onConnected(@Nullable Bundle bundle) {
+
+        locationRequest = new LocationRequest();
+
+        locationRequest.setInterval(interval);
+
+        locationRequest.setFastestInterval(100);
+
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+
+
+        if (ActivityCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(client, locationRequest, this);
+
+        }
+
+    }
+
+
+
+    @Override
+
+    public void onConnectionSuspended(int i) {
+
+    }
+
+
+
+    @Override
+
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
 }
