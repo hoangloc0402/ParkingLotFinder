3f2f97ddafc5b42cee0ada95d297933c8f5df715
tienbk97 <nghuuanhtien@gmail.com>
2018-01-20 12:21:33 +0700

    Maps to Fragment

--- app/src/main/java/hoangloc/parkinglotfinder/FragmentHome.java
+++ app/src/main/java/hoangloc/parkinglotfinder/FragmentHome.java
@@ -1,18 +1,28 @@
 package hoangloc.parkinglotfinder;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by NguyenHoangLoc on 1/20/2018.
  */
 
 
-public class FragmentHome extends Fragment {
+public class FragmentHome extends Fragment implements OnMapReadyCallback {
 
+    GoogleMap mMap;
 
     public FragmentHome() {
         // Required empty public constructor
@@ -22,8 +32,23 @@ public class FragmentHome extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home,null);
 
-
         return v;
 }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
